2853d365335f15a487e01945fab27bc7f3cb1405
Blackshadow-1007 <cs502762@gmail.com>
2026-05-11 00:41:50 +0200

    Moved Stuff around in repo
    All data is now public without having to unzip the pack

---
+++ Stuff/Auto_Zip_and_taco - Copy.bat
@@ -0,0 +1,13 @@
+:: You must replace YOUR PACKS NAME and YOUR XMLS NAME in order to use
+:: ex: del ".\TehsTrails.taco"
+
+:: deletes existing pack
+del "..\ShadowTrails.taco"
+
+:: uses WinRar to create a zip out of \Data and \YOUR XML
+"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\ShadowTrails.zip" ".\Data" ".\ShadowTrails.xml"
+
+:: renames the zip to taco
+rename "ShadowTrails.zip" "ShadowTrails.taco"
+
+move "ShadowTrails.taco" "..\"
